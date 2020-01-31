755ac73b3843e9724c2c10394b98d0c9c8031037
Roman Chursanov <authfailed@gmail.com>
2020-02-01 03:54:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from aiogram import Dispatcher, Bot
 
-token = '1071079532:AAEVzpm4Qs82jUyfnssbGksX3r8KO2YVN4k'
+token = 'your_token'
 bot = Bot(token=token, parse_mode='Markdown')
 dp = Dispatcher(bot)
